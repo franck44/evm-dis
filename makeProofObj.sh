d83a313f012ab616dce97a2c2771d49dd73d8632
franck44 <francksquash@gmail.com>
2024-01-08 22:03:19 +1100

    Add script to generate proof objects.

---
+++ makeProofObj.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Processing file: " $1
+
+filename=$1
+shortname=$(basename -- "$filename")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+echo "Shortname: " $shortname
+java -jar build/libs/Driver-java/evmdis.jar --title $shortname -p --cfg 100 --raw --lib ../../../../../evm-dafny $(<$1) >$filename-proof.dfy
+
