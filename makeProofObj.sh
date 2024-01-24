f4c9a30885adc727246757ed46bc39b6067849ae
franck44 <francksquash@gmail.com>
2024-01-24 15:32:37 +1100

    Disable formatting (crashes for large files).

--- makeProofObj.sh
+++ makeProofObj.sh
@@ -9,5 +9,5 @@ filename="${filename%.*}"
 
 echo "Shortname: " $shortname
 java -jar build/libs/Driver-java/evmdis.jar --title $shortname -p  --cfg 100 --raw --lib ../../../../../evm-dafny $(<$1) >$filename-proof.dfy
-dafny format $filename-proof.dfy
+# dafny format $filename-proof.dfy
 
