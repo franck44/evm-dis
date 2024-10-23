c574d95fb3133fcc9a2146804b87e5f84ba21a7a
franck44 <francksquash@gmail.com>
2024-10-23 19:14:16 +1100

    Simplify verify script.

--- verifyCFG.sh
+++ verifyCFG.sh
@@ -1,20 +1,35 @@
+#!/bin/bash
 
+# usage ./verifyCFG.sh <filename> <segment size>?
 echo "Processing file: " $1
 
 filename=$1
 shortname=$(basename -- "$filename")
+shortname="${shortname%.*}"
 extension="${filename##*.}"
 filename="${filename%.*}"
-segsize="10" 
+segsize="20" 
 
-echo "Shortname: " $shortname
+# output file, dafny code
+output="build/proofs/$shortname/$shortname-cfg-verifier.dfy"
+# create the target dir if it does not exist 
+mkdir -p build/proofs/$shortname
+
+# output file location
+echo "Shortname: " $shortname "// resulting file: " $output
+
+# if no segment size provided use default 10
 if [ "$#" -eq 2 ]; then
     segsize=$2
 fi
 echo "seg size: $segsize"
 
-java -jar build/libs/Driver-java/evmdis.jar --title $shortname -p  --size $segsize --cfg 100 --raw --lib ../../../../../evm-dafny $(<$1) >$filename-cfg-verifier.dfy
-dafny format  $filename-cfg-verifier.dfy
-dafny verify --allow-warnings --verification-time-limit 200 --isolate-assertions $filename-cfg-verifier.dfy
+# run evmdis to generate the dafny code
+java -jar build/libs/Driver-java/evmdis.jar --title $shortname -p  --size $segsize --cfg 100 --raw $(<$1) >$output
+
+# format the code
+dafny format  $output
+# verify with dafny 
+dafny verify --allow-warnings --verification-time-limit 200  $output
 
 
