59e65e01ccc4c3375d00e92652399a03b3c48188
franck44 <francksquash@gmail.com>
2024-10-23 19:39:58 +1100

    Update CFG generation script

--- makeCFG.sh
+++ makeCFG.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 
+# usage ./makeCFG.sh <filename> <segment size>?
 echo "Processing file: " $1
 
 filename=$1
@@ -6,7 +8,19 @@ shortname=$(basename -- "$filename")
 extension="${filename##*.}"
 filename="${filename%.*}"
 
+# output file, dafny code
+output="build/dot/$shortname/$shortname.dot"
+# create the target dir if it does not exist 
+mkdir -p build/dot/$shortname
+
+depthsize="100" 
+# if no max size provided use default 100
+if [ "$#" -eq 2 ]; then
+    depthsize=$2
+fi
+echo "Max depth size: $depthsize"
+
 echo "Shortname: " $shortname
-java -jar build/libs/Driver-java/evmdis.jar --title $shortname --cfg 100 $(<$1) >$filename-cfg.dot
-dot -Tsvg $filename-cfg.dot -o $filename-cfg.svg
+java -jar build/libs/Driver-java/evmdis.jar --title $shortname --cfg $depthsize $(<$1) >$output
+#dot -Tsvg $filename-cfg.dot -o $filename-cfg.svg
 
