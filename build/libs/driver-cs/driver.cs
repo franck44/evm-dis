1f7f9dd513b77985808b0a3ab6b81c23a2761fea
franck44 <francksquash@gmail.com>
2024-11-04 10:46:23 +1100

    Update artefacts (compiled with Dafny 4.9.0)

--- build/libs/driver-cs/driver.cs
+++ build/libs/driver-cs/driver.cs
@@ -8,7 +8,7 @@
 using System;
 using System.Numerics;
 using System.Collections;
-[assembly: DafnyAssembly.DafnySourceAttribute(@"// dafny 4.8.1.0
+[assembly: DafnyAssembly.DafnySourceAttribute(@"// dafny 4.9.0.0
 // Command-line arguments: build --no-verify --target cs --output build/libs/driver-cs/driver --function-syntax 4 --quantifier-syntax 4 src/dafny/Driver.dfy
 // Driver.dfy
 
@@ -8748,6 +8748,7 @@ internal static class FuncExtensions {
     return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16) => ResConv(F(ArgConv1(arg1), ArgConv2(arg2), ArgConv3(arg3), ArgConv4(arg4), ArgConv5(arg5), ArgConv6(arg6), ArgConv7(arg7), ArgConv8(arg8), ArgConv9(arg9), ArgConv10(arg10), ArgConv11(arg11), ArgConv12(arg12), ArgConv13(arg13), ArgConv14(arg14), ArgConv15(arg15), ArgConv16(arg16)));
   }
 }
+// end of class FuncExtensions
 #endif
 namespace _System {
 
@@ -11916,6 +11917,7 @@ internal static class FuncExtensions {
     return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) => ResConv(F(ArgConv1(arg1), ArgConv2(arg2), ArgConv3(arg3), ArgConv4(arg4), ArgConv5(arg5), ArgConv6(arg6), ArgConv7(arg7), ArgConv8(arg8)));
   }
 }
+// end of class FuncExtensions
 namespace MiscTypes {
 
   public partial class __default {
