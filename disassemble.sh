30fe50d24c9169a62adfbec604d5f602372c3fcd
franck44 <francksquash@gmail.com>
2024-11-03 16:38:26 +1100

    Update disassemble script.

--- disassemble.sh
+++ disassemble.sh
@@ -9,6 +9,5 @@ filename="${filename%.*}"
 
 echo "Shortname: " $shortname
 java -jar build/libs/Driver-java/evmdis.jar --title $shortname --dis $(<$1) 
-# dafny format $filename-verifier.dfy
-# dafny /dafnyVerify:1 /compile:0 /traceTimes /tracePOs $filename-cfg-verifier.dfy
+
 
