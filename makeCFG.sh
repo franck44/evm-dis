cd4c7fa04b799a5b4d52a8c63a4a0cbdec047aa9
franck44 <francksquash@gmail.com>
2024-11-04 09:22:57 +1100

    Fix shortname in script.

--- makeCFG.sh
+++ makeCFG.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# usage ./makeCFG.sh <filename> <segment size>?
+# usage ./makeCFG.sh <filename> <depth>?
 echo "Processing file: " $1
 
 filename=$1
@@ -9,19 +9,20 @@ extension="${filename##*.}"
 filename="${filename%.*}"
 
 # output file, dafny code
-output="build/dot/$shortname/$shortname.dot"
+shortnamenoext="${shortname%.*}"
+echo $shortnamenoext
+output="build/dot/$shortnamenoext/$shortnamenoext.dot"
 # create the target dir if it does not exist 
-mkdir -p build/dot/$shortname
+mkdir -p build/dot/$shortnamenoext
 
 depthsize="100" 
-# if no max size provided use default 100
+# if no max depth provided use default 100
 if [ "$#" -eq 2 ]; then
     depthsize=$2
 fi
 echo "Max depth size: $depthsize"
-
 echo "Shortname: " $shortname
-java -jar build/libs/Driver-java/evmdis.jar --title $shortname --cfg $depthsize $(<$1) >$output
+java -jar build/libs/Driver-java/evmdis.jar --title $shortnamenoext --cfg $depthsize $(<$1) >$output
 echo "CFG (DOT) format generared in $output"
 
 
