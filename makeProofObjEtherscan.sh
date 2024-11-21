5ddeed1a450512581f5a0df9149d9bbc9a13695b
franck44 <francksquash@gmail.com>
2024-11-21 15:40:25 +1100

    Fix usage of sed that differs on Linux and MacOS

--- makeProofObjEtherscan.sh
+++ makeProofObjEtherscan.sh
@@ -10,7 +10,7 @@ while read line; do # 'line' is the variable name
     filename=${line:0:42}
     mkdir -p build/proofs/etherscan/$filename
     java -jar build/libs/Driver-java/evmdis.jar --title $shortname -p  --size $2 --cfg 100 --raw --lib ../../../../../evm-dafny $(<etherscan/$filename/bytecode_$filename.evm) >build/proofs/etherscan/$filename/$filename-cfg-verifier.dfy
-    sed -i '' -e 's/include\ \"/include\ \"..\//g' build/proofs/etherscan/$filename/$filename-cfg-verifier.dfy
+    sed -i='' -e 's/include\ \"/include\ \"..\//g' build/proofs/etherscan/$filename/$filename-cfg-verifier.dfy
     dafny format build/proofs/etherscan/$filename/$filename-cfg-verifier.dfy
     # dafny verify --cores=12 --error-limit 1 build/proofs/etherscan/$filename/$filename-cfg-verifier.dfy
     if [ $? -eq 0 ] 
